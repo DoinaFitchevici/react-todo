7199b7c4650846fcea193923f47e40dc581b0e60
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-09-30 03:02:43 -0400

    Added list in React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 
+const todoList = [
+  {
+    id: 0,
+    title: "Read Road to React book",
+  },
+  {
+    id: 1,
+    title: "Complete first project assignment",
+  },
+  {
+    id: 2,
+    title: "Start working on second project",
+  },
+  {
+    id: 3,
+    title: "Practice coding",
+  },
+];
+
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-
-      <ul></ul>
+      <ul>
+        {todoList.map(function (todoItem) {
+          return <li key={todoItem.id}>{todoItem.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
