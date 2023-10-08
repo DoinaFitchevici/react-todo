3b5bcf8b30dc453793f0bd4ac5d501499c46f066
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-10-08 00:27:12 -0400

    Moved list into new TodoList component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from "react";
-
-const todoList = [
-  {
-    id: 0,
-    title: "Read Road to React book",
-  },
-  {
-    id: 1,
-    title: "Complete first project assignment",
-  },
-  {
-    id: 2,
-    title: "Start working on second project",
-  },
-  {
-    id: 3,
-    title: "Practice coding",
-  },
-];
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todoItem) => (
-          <li key={todoItem.id}>{todoItem.title}</li>
-        ))}
-      </ul>
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const todoList = [
+  {
+    id: 0,
+    title: "Read Road to React book",
+  },
+  {
+    id: 1,
+    title: "Complete first project assignment",
+  },
+  {
+    id: 2,
+    title: "Start working on second project",
+  },
+  {
+    id: 3,
+    title: "Practice coding",
+  },
+];
+
+function TodoList() {
+  return (
+    <div>
+      <ul>
+        {todoList.map((todoItem) => (
+          <li key={todoItem.id}>{todoItem.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default TodoList;
