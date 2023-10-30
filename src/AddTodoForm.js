4700111163e1a67f70a27409aea8690f8af48c71
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-10-30 00:11:32 -0400

    Controlled AddTodo input

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddTodoForm(props) {
-  function handleAddTodo(event) {
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleTitleChange = (event) => {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  };
+
+  const handleAddTodo = (event) => {
     event.preventDefault();
     console.log(event.target);
-    const todoTitle = event.target.title.value;
+
     console.log("value is", todoTitle);
     props.onAddTodo(todoTitle);
     event.target.reset();
-  }
+  };
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input name="title" type="text" id="todoTitle" />
+      <input
+        name="title"
+        type="text"
+        id="todoTitle"
+        value={todoTitle}
+        onChange={handleTitleChange}
+      />
       <button type="submit">Add</button>
     </form>
   );
