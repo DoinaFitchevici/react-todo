b9d0d8503bac28a3d9093b12ed0fa3727380d2cc
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2023-11-11 17:54:23 -0500

    Added autoFocus to input

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,8 +1,21 @@
+import { useRef, useEffect } from "react";
+
 const InputWithLabel = ({ id, children, type, value, onChange }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
   return (
     <>
       <label htmlFor={id}>{children}</label>
-      <input type={type} id={id} value={value} onChange={onChange} />
+      <input
+        ref={inputRef}
+        type={type}
+        id={id}
+        value={value}
+        onChange={onChange}
+      />
     </>
   );
 };
