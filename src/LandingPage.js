d011fdb3495e58f54966e537af913dac579e80e7
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2024-02-13 06:28:27 -0500

    Another name

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -6,10 +6,8 @@ const LandingPage = () => {
     <>
       <main>
         <div className={styles.container}>
-          <h1>Welcome to Your Busy Bee List App</h1>
-          <p>
-            Buzzing Through Your Tasks with Your Personal Hive of Productivity
-          </p>
+          <h1>Welcome to Your Task Tracker App</h1>
+          <p>Your Guide to Productivity, One Checkmark at a Time!</p>
         </div>
         <div className={styles.buttonContainer}>
           <Link to="/TodoList" className={styles.button}>
@@ -21,7 +19,7 @@ const LandingPage = () => {
         </div>
       </main>
       <footer className={styles.footer}>
-        <p>© 2024 Your Busy Bee List App. All rights reserved.</p>
+        <p>© 2024 Your Task Tracker App. All rights reserved.</p>
       </footer>
     </>
   );
