0462b093caebaadfc47b47a6e4ae925dd3945c82
Doina Fitchevici <doinafitchevici@Doinas-MBP.home>
2024-02-13 06:16:22 -0500

    Changed name of app

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -6,8 +6,10 @@ const LandingPage = () => {
     <>
       <main>
         <div className={styles.container}>
-          <h1>Welcome to My Todo App</h1>
-          <p>Organize your tasks with ease.</p>
+          <h1>Welcome to Your Busy Bee List App</h1>
+          <p>
+            Buzzing Through Your Tasks with Your Personal Hive of Productivity
+          </p>
         </div>
         <div className={styles.buttonContainer}>
           <Link to="/TodoList" className={styles.button}>
@@ -19,7 +21,7 @@ const LandingPage = () => {
         </div>
       </main>
       <footer className={styles.footer}>
-        <p>© 2024 Your Todo App. All rights reserved.</p>
+        <p>© 2024 Your Busy Bee List App. All rights reserved.</p>
       </footer>
     </>
   );
